10973384e9895767a3e05345deac09bd7d41164a
shiroe66 <shiroeqky@gmail.com>
2022-12-02 22:02:27 +0500

    feat: swagger module

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { AppModule } from '@app/app.module';
 import { AppConfigService } from '@app/config/app/config.service';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig: AppConfigService = app.get(AppConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Accounting')
+    .setDescription('Accounting for your income and expenses API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(appConfig.port());
   console.log(`Server started at ${appConfig.url()} 🚀`);
